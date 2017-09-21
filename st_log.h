74c9be0068174e8329ddb6e6d8e218619d85dbfd
NiroShea <nexusim@163.com>
2017-09-21 10:59:10 +0800

    this is the first banben.

---
+++ st_log.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#ifndef _ST_LOG_H__
+#define __ST_LOG_H__
+
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <aio.h>
+
+typedef enum {  
+    DEBUG,  
+    INFO,  
+    WARN,  
+    ERROR,  
+    OFF  
+}LOG_LEVEL; 
+
+//写日志
+int stLog(LOG_LEVEL level,int pid,char* fmt,...);
+
+//日志系统初始化
+void log_init();
+
+#ifdef  __cplusplus
+}
+#endif
+#endif
