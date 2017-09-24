5e2cd7d589d1f7ee51b693f1aafb41dd59620e63
NiroShea <nexusim@163.com>
2017-09-24 21:11:59 +0800

    big modify!

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
-#include "hrbb_log.h"
-//#include <sys/stat.h> 
-//static pthread_t t_1;  
-//static pthread_t t_2;
+#include "st_log.h"
 
 void *funcA(void *p){
 	int count = *((int*)p);
 	while(1){
-		stLog(WARN,count,"%s%s","zhe shi yi tiao WARN!=======================================","hfdkjahkfhakshdfjk");
+		stLog(YYJYLS,count,"%s%s%d","zhe shi yi tiao YYJYLS!==================AHSDKFJHAKJSDHFJKASDF65456454=====================","=======hfdkjahkfhakshdfjk===",count);
+		sleep(1);
+	}
+}
+
+void *funcB(void *p){
+	int count = *((int*)p);
+	while(1){
+		stLog(YYERROR,count,"%s%s%d","zhe shi yi tiao YYERROR!=========SDF88888888888888888888888888888888888888888888888888==========","=======0hf6545466dsdfsdxxxx==",count);
 		sleep(1);
 	}
 }
 
 int main(int argc,char** argv){
 	log_init("log.config");
-	int a = 16;
-	int count = 3;
-	
+		
 	int i = 0;
-	for(; i < 10 ; i++){
+	for(; i < 1000 ; i++){
 		pthread_t t_1; 
 		int *p = malloc(sizeof(int));
 		*p = i;
@@ -25,6 +28,17 @@ int main(int argc,char** argv){
 			printf("funcA_t create failed!\n"); 
 		}
 	}
+		
+	int j = 1000;
+	for(;j < 2000 ; j++){
+		pthread_t t_2;
+		int *m = malloc(sizeof(int));
+		*m = j;
+		if(pthread_create(&t_2,NULL,funcB,(void*)m)){
+			printf("sdfjksjdfkjksdjfk!");
+		}
+	}
+
 	
 	while(1){
 		sleep(5);
--- st_log.c
+++ st_log.c
@@ -6,7 +6,7 @@ const static int MaxLine = 1024;
 //日志文件路径，请在配置文件中配置，覆盖默认设置
 static char LOG_FILE_PATH[256] = "/tmp/";
 //使用该插件的应用名称，在配置文件中配置
-static char APP_NAME[64] = "appName";
+static char APP_NAME[256] = "appName";
 //日志的配置文件路径
 static char LOG_CONF_PATH[128] = "/tmp";
 //日志初始化线程
@@ -34,50 +34,76 @@ typedef struct threadArg{
 	int ts;
 }tArg;
 
+void *moLogfile(void *arg);
+
+//日志等级转换成字符串
+char *log_level_2_str(LOG_LEVEL level){  
+    switch (level)  
+    {  
+    case YYJYLS:  
+        return (char*)"YYJYLS";  
+    case YYERROR:  
+        return (char*)"YYERROR";  
+    default:  
+        return (char*)"UNKNOW";  
+    }  
+    return (char*)"UNKNOW";  
+}
+
 //日志系统初始化
 void log_init(char * logConfPath){
 
 	//获取日志配置文件路径
-	memcpy(FILE_ABS_PATH,logConfPath,strlen(logConfPath));
+	memcpy(LOG_CONF_PATH,logConfPath,strlen(logConfPath));
 	memset(LOG_ABS_PATH_JYLS, 0, LOGNUM);
 	memset(LOG_ABS_PATH_ERROR, 0, LOGNUM);
 	char buffer[CONFLEN];
 	if(getValue("LOG_FILE_PATH",buffer) == 1){
+		int i,j;
 		char str[LOGNUM];
-		if(buffer[strlen(buffer)-1] != '/'){
-			strcat(str,buffer);
-			strcat(str,"/");
-		}
+		memset(str,0,LOGNUM);
 		memset(LOG_FILE_PATH,0,LOGNUM);
-		memcpy(LOG_FILE_PATH,str,strlen(str));
-		printf("LOG_FILE_PATH_OK---->%s\n",LOG_FILE_PATH);
+		for(i = 0,j = 0;i < strlen(buffer);i++){
+			if(buffer[i] == ' '){
+				continue;
+			}else{
+				str[j] = buffer[i];
+				j++;
+			}
+		}
+		str[j] = '\0';
+		snprintf(LOG_FILE_PATH,LOGNUM,"%s%s",str,"/");
+		//printf("LOG_FILE_PATH_OK---->%s\n",LOG_FILE_PATH);
 	}
 	if(getValue("APP_NAME",buffer) == 1){
-		memset(APP_NAME,0,64);
-		memcpy(APP_NAME,buffer,strlen(buffer));
-		printf("FILE_SIZE_NUM_OK---->%ld\n",APP_NAME);
+		memset(APP_NAME,0,LOGNUM);
+		snprintf(APP_NAME,LOGNUM,"%s",buffer);
+		//printf("APP_NAME_OK---->%s\n",APP_NAME);
 	}
+
 	if(getValue("FILE_SIZE_NUM",buffer) == 1){
 		int tmp = atoi(buffer);
+			
 		if(tmp > 0)FILE_SIZE_NUM = tmp;
-		printf("FILE_SIZE_NUM_OK---->%ld\n",FILE_SIZE_NUM);
+		
+		//printf("FILE_SIZE_NUM_OK---->%d\n",FILE_SIZE_NUM);
+		
 	}
 	get_local_time(LOG_TIME_JYLS);
 	get_local_time(LOG_TIME_ERROR);
 
-
-	tArg *x;
-	x.ts = 36;
-	if(pthread_create(&t_1,NULL,moLogfile,x)){
-		printf("funcA_t create failed!\n"); 
-	}
+	//tArg *x;
+	//x->ts = 36;
+	int x = 1;
+	if(pthread_create(&t_1,NULL,moLogfile,&x))perror("funcA_t create failed!\n");
 
 	char log_tmp_path[LOGNUM]; 
 	char log_tmp_path2[LOGNUM];
-	memcpy(log_tmp_path,LOG_FILE_PATH,strlen(LOG_FILE_PATH));
-	memcpy(log_tmp_path2,LOG_FILE_PATH,strlen(LOG_FILE_PATH));
-	strcat(log_tmp_path,"YYERROR/");
-	strcat(log_tmp_path2,"YYJYLS/");
+	memset(log_tmp_path,0,LOGNUM);
+	memset(log_tmp_path2,0,LOGNUM);
+	snprintf(log_tmp_path,LOGNUM,"%s%s",LOG_FILE_PATH,"YYERROR/");
+	snprintf(log_tmp_path2,LOGNUM,"%s%s",LOG_FILE_PATH,"YYJYLS/");
+
 	if(!opendir(log_tmp_path))createDir(log_tmp_path);
 	if(!opendir(log_tmp_path2))createDir(log_tmp_path2);
 }
@@ -128,7 +154,7 @@ int o_write_file(LOG_LEVEL level,char* buffer){
 	
 	get_local_time(now);
 	
-	snprintf(s_buf,LOG_SIZE,"%s_%s",now,buffer);
+	snprintf(s_buf,LOG_SIZE,"%s %s",now,buffer);
 	
 	int f = -1;
 	if(level == YYJYLS){
@@ -166,7 +192,7 @@ int aio_w_file(char * const filename,char * const str){
 	memset((char*)cblist,0,sizeof(cblist));
 	cblist[0] = &my_aio;
 	
-	int fd = open(filename, O_WRONLY | O_APPEND | O_CREAT , 0666);
+	int fd = open(filename, O_WRONLY | O_APPEND | O_CREAT , 0644);
 	if(fd < 0)return 2;
 	
 	my_aio.aio_fildes = fd;
@@ -186,70 +212,23 @@ int aio_w_file(char * const filename,char * const str){
 	return 1;
 }
 
-//检测文件大小，如果过大获取新的日志文件名
-void *moLogfile(void *arg){
-	int count1 = 1 ;
-	int count2 = 1 ;
-	while(1){
-		//该线程函数管理日志名
-		snprintf(LOG_ABS_PATH_JYLS, LOGNUM ,"%s%s%s_%s_%s_%04d.log",LOG_FILE_PATH,"YYJYLS/",APP_NAME,"YYJYLS",LOG_TIME_JYLS,count1);
-		snprintf(LOG_ABS_PATH_ERROR, LOGNUM ,"%s%s%s_%s_%s_%04d.log",LOG_FILE_PATH,"YYERROR/",APP_NAME,"YYERROR",LOG_TIME_ERROR,count2);
-		//snp_str_log(loglevel,LOG_TIME,logname);
-		
-		//计算长度
-		long length1 = 0;
-		long length2 = 0;
-		FILE *fp = NULL;
-		
-		fp = fopen(LOG_ABS_PATH_JYLS, "rb");
-		if (fp != NULL){
-			fseek(fp, 0, SEEK_END);
-			length1 = ftell(fp);
-			fclose(fp);
-			fp = NULL;
-		}
-		
-		fp = fopen(LOG_ABS_PATH_ERROR,"rb");
-		if (fp != NULL){
-			fseek(fp, 0, SEEK_END);
-			length2 = ftell(fp);
-			fclose(fp);
-			fp = NULL;
-		}
-		
-		//printf("111111111111====> File is too large,start to split.\n");
-		
-		tArg *x;
-		x = (tArg *)arg;
-
-		printf("111111111111====> %s\n",LOG_ABS_PATH_JYLS);
-		printf("111111111111====> %s\n",LOG_ABS_PATH_ERROR);
-		printf("0000000000000000000110101010111===>%d\n", x->ts);
-		
-		if ( length1 > FILE_SIZE_NUM * FILE_MAX_SIZE ){//检测到文件太大
-			count1++;
-			get_local_time(LOG_TIME_JYLS);
-		}
-		if ( length2 > FILE_SIZE_NUM * FILE_MAX_SIZE ){//检测到文件太大
-			count2++;
-			get_local_time(LOG_TIME_ERROR);
-		}
-		sleep(3);
-	}
-}
-
 //级联创建文件夹
-int createDir(const char *sPathName){  
+int createDir(const char *sPathName){
+	
+	
+	
 	char DirName[256];  
 	strcpy( DirName,sPathName);  
 	int i,len = strlen(DirName);  
+	
 	if(DirName[len-1]!='/')strcat(DirName, "/");  
 	len = strlen(DirName);  
+
 	for(i=1;   i<len;   i++){  
 		if(DirName[i]=='/'){
-			DirName[i] = 0;  
+			DirName[i] = 0;
 			if( access(DirName, W_OK) != 0   ){  
-				if(mkdir(DirName,   0755)==-1){   
+				if(mkdir(DirName,   0644)==-1){   
 					perror("mkdir   error");   
 					return   -1; 
 				}
@@ -262,7 +241,8 @@ int createDir(const char *sPathName){
 
 //获取本地时间
 int get_local_time(char * buffer){
-	memset(buffer, 0, sizeof(buffer));
+	
+	memset(buffer, 0, TNUM);
 	time_t rawtime;
 	struct tm* timeinfo;
 	time(&rawtime);
@@ -315,7 +295,7 @@ int intput_config_value(char *file_absolute_path, char *key, char **value, int *
         //读取行
         tempP = fgets(lineBuf,MaxLine,file);
         
-		if(*lineBuf == '#' || *lineBuf == ' '){
+		if(*lineBuf == '#' || *lineBuf == ' ' || *lineBuf == '\n'){
 			continue;
 		}
 		
@@ -353,19 +333,19 @@ int intput_config_value(char *file_absolute_path, char *key, char **value, int *
             }
         }
         while (1) {
-            if ((*tempP == ' ' || *tempP == '\n')) {
-                break;
-            }
-            else {
-                tempP++;
-            }
+			if(*tempP == ' ' || *tempP == '\n' || *tempP == '\r')break;
+			tempP++;
         }
         end = tempP;
+		*tempP = '\0';
         //计算出value长度
-        *len = end - start;
+        *len = end - start + 1;
         tempValue = (char*)malloc((*len) * sizeof(char));
-        memcpy(tempValue, start, (*len));
-        *value = tempValue;
+		int s = 0;
+		for(;s < *len ; s++){
+			tempValue[s] = start[s];
+		}
+		*value = tempValue;
 		if(*value == "" || *value == NULL){
 			ret = 0;
 		}else{
@@ -373,6 +353,63 @@ int intput_config_value(char *file_absolute_path, char *key, char **value, int *
 			return ret;
 		}
     }
-
     return ret;
+}
+
+//检测文件大小，如果过大获取新的日志文件名
+void *moLogfile(void *arg){
+	int count1 = 1 ;
+	int count2 = 1 ;
+	while(1){
+		//该线程函数管理日志名
+		//printf("start!\n");
+		memset(LOG_ABS_PATH_JYLS,0,LOGNUM);
+		memset(LOG_ABS_PATH_ERROR,0,LOGNUM);
+
+		snprintf(LOG_ABS_PATH_JYLS, LOGNUM ,"%s%s%s_%s_%s_%04d.log",LOG_FILE_PATH,"YYJYLS/",APP_NAME,"YYJYLS",LOG_TIME_JYLS,count1);
+		snprintf(LOG_ABS_PATH_ERROR, LOGNUM ,"%s%s%s_%s_%s_%04d.log",LOG_FILE_PATH,"YYERROR/",APP_NAME,"YYERROR",LOG_TIME_ERROR,count2);
+		//snp_str_log(loglevel,LOG_TIME,logname);
+		//printf("LOG_ABS_PATH_JYLS================>%s\n",LOG_ABS_PATH_JYLS);
+		//printf("LOG_ABS_PATH_ERROR=============>%s\n",LOG_ABS_PATH_ERROR);
+
+		//计算长度
+		long length1 = 0;
+		long length2 = 0;
+		FILE *fp = NULL;
+		
+		fp = fopen(LOG_ABS_PATH_JYLS, "rb");
+		if (fp != NULL){
+			fseek(fp, 0, SEEK_END);
+			length1 = ftell(fp);
+			fclose(fp);
+			fp = NULL;
+		}
+		
+		fp = fopen(LOG_ABS_PATH_ERROR,"rb");
+		if (fp != NULL){
+			fseek(fp, 0, SEEK_END);
+			length2 = ftell(fp);
+			fclose(fp);
+			fp = NULL;
+		}
+		
+		//printf("111111111111====> File is too large,start to split.\n");
+		
+ 		//tArg *x;
+		//x = (tArg *)arg;
+
+		//printf("-=s-df-sdfx%s\n",LOG_ABS_PATH_JYLS);
+		//printf("-=s-df-sdfx%s\n",LOG_ABS_PATH_ERROR);
+		//printf("0000000000000000000110101010111===>%d\n", 1);
+		
+		if ( length1 > FILE_SIZE_NUM * FILE_MAX_SIZE ){//检测到文件太大
+			count1++;
+			get_local_time(LOG_TIME_JYLS);
+		}
+		if ( length2 > FILE_SIZE_NUM * FILE_MAX_SIZE ){//检测到文件太大
+			count2++;
+			get_local_time(LOG_TIME_ERROR);
+		}
+		sleep(6);
+	}
 }
